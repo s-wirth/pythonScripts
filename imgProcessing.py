6e7a70480f7f5808f7768936d1d033ba56875e14
Sophie Wirth <sophiewirth@macbook-pro.speedport.ip>
2025-06-11 11:31:46 +0200

    img processing script

---
+++ imgProcessing.py
@@ -0,0 +1,64 @@
+import cv2
+import sys
+import inspect
+
+#######################################################################
+# Image Processing Functions                                  
+#######################################################################
+def blurImage(file):
+    """Blurs an image using Gaussian blur."""
+    img = cv2.imread(cv2.samples.findFile(file))
+
+    if img is None:
+        sys.exit("Could not read the image.")
+    else:
+        imgName = file.split(".")[0]
+        blurName = 'blurred_' + imgName + '.jpg'
+        blur = cv2.GaussianBlur(img, (451, 451), 0)
+        cv2.imwrite(blurName, blur)
+    return
+    
+def blurAllImages(dirName):
+    """Creates a blurred image using Gaussian blur for each image in the directory."""
+    return
+
+#######################################################################
+# Find and Display Functions                                  
+#######################################################################
+
+def _get_local_functions():
+    local_functions = {}
+    for name, obj in inspect.getmembers(sys.modules[__name__]):
+        if inspect.isfunction(obj) and not name.startswith('_') and obj.__module__ == __name__:
+            local_functions[name] = obj
+    return local_functions
+
+
+def _list_functions(script_name):
+    print(f"Available functions in {script_name}:")
+    for name, f in _get_local_functions().items():
+        print()
+        arguments = inspect.signature(f).parameters
+        print(f"{name}")
+        print(f"arguments: {' '.join(arguments)}")
+        print(f"returns: {f.__doc__ or '?'}")
+        print(f"Command: python3 {script_name} {name} <{' '.join(arguments)}>")
+
+
+#######################################################################
+# Entry Point                                 
+#######################################################################
+
+if __name__ == '__main__':
+    script_name, *args = sys.argv
+    if args:
+        functions = _get_local_functions()
+        function_name = args.pop(0)
+        if function_name in functions:
+            function = functions[function_name]
+            function(*args)
+        else:
+            print(f"Function {function_name} not found")
+            _list_functions(script_name)
+    else:
+        _list_functions(script_name)
